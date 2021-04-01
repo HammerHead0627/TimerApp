2b52f880c39afddfde825b260459c21f76af3409
HammerHead <legox06270627@gmail.com>
2021-04-02 06:06:36 +0900

    add startTimer()

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -11,11 +11,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //秒数
+    //ミリ秒単位で設定
     private static final long START_TIME = 10000;
 
     //view
-    private TextView text;
+    private TextView timerText;
     private Button button_start_pause;
     private Button button_reset;
 
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     //タイマー稼働フラグ
     private boolean timerRunning;
 
+    //タイマー初期値
     private long timeLeftMills = START_TIME;
 
 
@@ -31,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //表示時間
+        timerText = findViewById(R.id.text_view_countdown);
 
         //クリックイベントを取得したいボタン
         button_start_pause = findViewById(R.id.button_start_pause);
@@ -41,17 +45,40 @@ public class MainActivity extends AppCompatActivity {
                 //クリック時に実行
                 @Override
                 public void onClick(View view){
-                startTimer();
+                    //タイマーが稼働している場合、一時停止する
+                    if(timerRunning){
+                        stopTimer();
+                    }else{
+                        startTimer();
+                    }
         }
         });
 
-        //button_reset.setOnClickListener({});
+        button_reset.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    resetTimer();
+            }
+        });
 
+        updateCountdownText();
     }
 
     //タイマーを開始する
     private void startTimer(){
-
+        countDownTimer  = new CountDownTimer(timeLeftMills,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftMills = millisUntilFinished;
+                updateCountdownText();
+            }
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+                button_start_pause.setText("スタート");
+                button_reset.setVisibility(View.INVISIBLE);
+            }
+        }.start();
     }
     //一時停止
     private void stopTimer(){
@@ -64,6 +91,15 @@ public class MainActivity extends AppCompatActivity {
 
     //時刻を表示する
     private void updateCountdownText(){
+        //初期値を秒にし、60で割って分とする
+        int minutes = (int)((timeLeftMills)/1000)/60;
+        //初期値を秒にし、60で割って剰余を秒とする
+        int seconds = (int)((timeLeftMills)/1000)%60;
 
+        //フォーマットを整える
+        String timerLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        //テキスト部に表示する
+        timerText.setText(timerLeftFormatted);
     }
 }
