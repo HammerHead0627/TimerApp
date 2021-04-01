142c0a0b2e49fc2fecd17c63bbaf98919920f20f
HammerHead <legox06270627@gmail.com>
2021-04-02 06:19:53 +0900

    add stoptimer() add resetTimer()

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.timerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+
 import java.util.Locale;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ミリ秒単位で設定
+    //最初の設定値をミリ秒単位で定義
     private static final long START_TIME = 10000;
 
     //view
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     //タイマー稼働フラグ
     private boolean timerRunning;
 
-    //タイマー初期値
+    //タイマーの初期値
     private long timeLeftMills = START_TIME;
 
 
@@ -41,23 +44,23 @@ public class MainActivity extends AppCompatActivity {
         button_reset = findViewById(R.id.button_reset);
 
         //OnClickListener インターフェースを実装する
-        button_start_pause.setOnClickListener(new View.OnClickListener(){
-                //クリック時に実行
-                @Override
-                public void onClick(View view){
-                    //タイマーが稼働している場合、一時停止する
-                    if(timerRunning){
-                        stopTimer();
-                    }else{
-                        startTimer();
-                    }
-        }
+        button_start_pause.setOnClickListener(new View.OnClickListener() {
+            //クリック時に実行
+            @Override
+            public void onClick(View view) {
+                //タイマーが稼働している場合、一時停止する
+                if (timerRunning) {
+                    stopTimer();
+                } else {
+                    startTimer();
+                }
+            }
         });
 
-        button_reset.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    resetTimer();
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
             }
         });
 
@@ -65,13 +68,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //タイマーを開始する
-    private void startTimer(){
-        countDownTimer  = new CountDownTimer(timeLeftMills,1000) {
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftMills, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 timeLeftMills = millisUntilFinished;
                 updateCountdownText();
             }
+
             @Override
             public void onFinish() {
                 timerRunning = false;
@@ -79,22 +83,39 @@ public class MainActivity extends AppCompatActivity {
                 button_reset.setVisibility(View.INVISIBLE);
             }
         }.start();
-    }
-    //一時停止
-    private void stopTimer(){
 
+        timerRunning = true;
+        button_start_pause.setText("一時停止");
+        button_reset.setVisibility(View.INVISIBLE);
     }
+
+    //一時停止
+    private void stopTimer() {
+
+        countDownTimer.cancel();
+        timerRunning = false;
+        button_start_pause.setText("スタート");
+        button_reset.setVisibility(View.VISIBLE);
+    }
+
     //リセット
-    private void resetTimer(){
+    private void resetTimer() {
+        //タイマーの設定値を初期値に戻す
+        timeLeftMills = START_TIME;
+        updateCountdownText();
+
+        //ボタンの表示を切り変える
+        button_start_pause.setVisibility(View.VISIBLE);
+        button_reset.setVisibility(View.INVISIBLE);
 
     }
 
     //時刻を表示する
-    private void updateCountdownText(){
+    private void updateCountdownText() {
         //初期値を秒にし、60で割って分とする
-        int minutes = (int)((timeLeftMills)/1000)/60;
+        int minutes = (int) ((timeLeftMills) / 1000) / 60;
         //初期値を秒にし、60で割って剰余を秒とする
-        int seconds = (int)((timeLeftMills)/1000)%60;
+        int seconds = (int) ((timeLeftMills) / 1000) % 60;
 
         //フォーマットを整える
         String timerLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
